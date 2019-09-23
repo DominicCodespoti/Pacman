f9f89631e05d8dd642e4abda9e0601c61ca677ce
DominicCodespoti <dominic.codespoti@myob.com>
2019-09-23 15:49:07 +1000

    Broadened test suite to ensure all specs of design are tested

--- src/main/java/View/Console/ConsoleCleanUp.java
+++ src/main/java/View/Console/ConsoleCleanUp.java
@@ -6,6 +6,7 @@ public class ConsoleCleanUp extends Thread {
   public void run(){
     String[] cmd = {"/bin/sh", "-c", "stty cooked </dev/tty"};
     try {
+      System.out.println("Console is now in cooked mode!");
       Runtime.getRuntime().exec(cmd).waitFor();
     } catch (InterruptedException | IOException e) {
       e.printStackTrace();
---
+++ src/test/java/DataStructuresTests/QuadruplyLinkedListTests.java
@@ -0,0 +1,5 @@
+package DataStructuresTests;
+
+public class QuadruplyLinkedListTests {
+
+}
